df78546c52d5c5e56704aaab92658e9edaffacbd
anushaGarg <anusha.garg@ayanworks.com>
2022-11-11 15:16:53 +0530

    Changed registry contract package version

--- tests/test.data.ts
+++ tests/test.data.ts
@@ -1,7 +1,7 @@
 
-export const testDid = 'did:polygon:testnet:0x794b781493AeD65b9ceBD680716fec257e118993';
+export const testDid = '';
 
-export const privateKey = "0x7cd54a79f153402e1a9349188a67b1c9df510c83d27922f50de119d8d787b4fa";
+export const privateKey = "";
 
 export const url = "https://rpc-mumbai.matic.today";
 
