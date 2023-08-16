eb8a367588bad6d075995c7c7ab23adf58baa024
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-08-16 13:08:28 +0530

    refactor: add pnpm and update package.json
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- src/polygon-did-resolver.ts
+++ src/polygon-did-resolver.ts
@@ -1,12 +1,17 @@
-import * as log4js from "log4js";
-import * as networkConfiguration from "./configuration.json";
-import { ethers } from "ethers";
-import { DIDDocument, DIDResolutionResult, DIDResolver, ParsedDID, Resolver } from 'did-resolver';
-const DidRegistryContract = require("@ayanworks/polygon-did-registry-contract");
+import * as log4js from 'log4js'
+import { Contract, JsonRpcProvider, ethers } from 'ethers'
+import {
+  DIDDocument,
+  DIDResolutionResult,
+  DIDResolver,
+  ParsedDID,
+  Resolver,
+} from 'did-resolver'
+import { networkConfig } from './config'
+const DidRegistryContract = require('@ayanworks/polygon-did-registry-contract')
 
-
-const logger = log4js.getLogger();
-logger.level = `debug`;
+const logger = log4js.getLogger()
+logger.level = `debug`
 
 /**
  * Resolves DID Document.
@@ -14,87 +19,86 @@ logger.level = `debug`;
  * @returns Return DID Document on chain.
  */
 
-export function getResolver (): Record<string, DIDResolver> {
-      async function resolve(did: string, parsed?: ParsedDID): Promise<DIDResolutionResult> {
+export function getResolver(): Record<string, DIDResolver> {
+  async function resolve(
+    did: string,
+    parsed?: ParsedDID,
+  ): Promise<DIDResolutionResult> {
+    const didDocumentMetadata = {}
+    try {
+      let errorMessage: string
+      let url: string
+      let contractAddress: string
+      const didWithTestnet: string = await splitPolygonDid(did)
 
-            const didDocumentMetadata = {}
-            try {
-                  let errorMessage: string;
-                  let url: string;
-                  let contractAddress: string;
-                  const didWithTestnet: string = await splitPolygonDid(did);
-      
-                  if (
-                        (did &&
-                              didWithTestnet === "testnet" &&
-                              did.match(/^did:polygon:testnet:0x[0-9a-fA-F]{40}$/)) ||
-                        (did && did.match(/^did:polygon:0x[0-9a-fA-F]{40}$/))
-                  ) {
-                        if (
-                              (didWithTestnet === "testnet" &&
-                                    did.match(/^did:polygon:testnet:\w{0,42}$/)) ||
-                              did.match(/^did:polygon:\w{0,42}$/)
-                        ) {
-                              if (did && didWithTestnet === "testnet") {
-                                    url = `${networkConfiguration[0].testnet?.URL}`;
-                                    contractAddress = `${networkConfiguration[0].testnet?.CONTRACT_ADDRESS}`;
-                              } else {
-                                    url = `${networkConfiguration[1].mainnet?.URL}`;
-                                    contractAddress = `${networkConfiguration[1].mainnet?.CONTRACT_ADDRESS}`;
-                              }
-      
-                              const provider: ethers.providers.JsonRpcProvider = new ethers.providers.JsonRpcProvider(
-                                    url
-                              );
-                              const registry: ethers.Contract = new ethers.Contract(
-                                    contractAddress,
-                                    DidRegistryContract.abi,
-                                    provider
-                              );
-      
-                              const didAddress: string =
-                                    didWithTestnet === "testnet" ? did.split(":")[3] : didWithTestnet;
-      
-                              // Calling smart contract with getting DID Document
-                              let didDocument: any = await registry.functions
-                                    .getDIDDoc(didAddress)
-                                    .then((resValue: any) => {
-                                          return resValue;
-                                    });
-      
-                              logger.debug(
-                                    `[resolveDID] readDIDDoc - ${JSON.stringify(didDocument)} \n\n\n`
-                              );
-      
-                              if (didDocument && !didDocument.includes("")) {
-                                    return{
-                                          didDocument,
-                                          didDocumentMetadata,
-                                          didResolutionMetadata: {contentType: 'application/did+ld+json'}
-                                    }
-                              } else {
-                                    errorMessage = `The DID document for the given DID was not found!`;
-                                    logger.error(errorMessage);
-                                    throw new Error(errorMessage);
-                              }
-                        } else {
-                              errorMessage = `Invalid address has been entered!`;
-                              logger.error(errorMessage);
-                              throw new Error(errorMessage);
-                        }
-                  } else {
-                        errorMessage = `Invalid DID has been entered!`;
-                        logger.error(errorMessage);
-                        throw new Error(errorMessage);
-                  }
-            } catch (error) {
-                  logger.error(`Error occurred in resolve function ${error}`);
-                  throw error;
+      if (
+        (did &&
+          didWithTestnet === 'testnet' &&
+          did.match(/^did:polygon:testnet:0x[0-9a-fA-F]{40}$/)) ||
+        (did && did.match(/^did:polygon:0x[0-9a-fA-F]{40}$/))
+      ) {
+        if (
+          (didWithTestnet === 'testnet' &&
+            did.match(/^did:polygon:testnet:\w{0,42}$/)) ||
+          did.match(/^did:polygon:\w{0,42}$/)
+        ) {
+          if (did && didWithTestnet === 'testnet') {
+            url = `${networkConfig.testnet?.URL}`
+            contractAddress = `${networkConfig.testnet?.CONTRACT_ADDRESS}`
+          } else {
+            url = `${networkConfig.mainnet?.URL}`
+            contractAddress = `${networkConfig.mainnet?.CONTRACT_ADDRESS}`
+          }
+
+          const provider = new JsonRpcProvider(url)
+          const registry = new Contract(
+            contractAddress,
+            DidRegistryContract.abi,
+            provider,
+          )
+
+          const didAddress: string =
+            didWithTestnet === 'testnet' ? did.split(':')[3] : didWithTestnet
+
+          // Calling smart contract with getting DID Document
+          let didDocument: any = await registry.functions
+            .getDIDDoc(didAddress)
+            .then((resValue: any) => {
+              return resValue
+            })
+
+          logger.debug(
+            `[resolveDID] readDIDDoc - ${JSON.stringify(didDocument)} \n\n\n`,
+          )
+
+          if (didDocument && !didDocument.includes('')) {
+            return {
+              didDocument,
+              didDocumentMetadata,
+              didResolutionMetadata: { contentType: 'application/did+ld+json' },
             }
+          } else {
+            errorMessage = `The DID document for the given DID was not found!`
+            logger.error(errorMessage)
+            throw new Error(errorMessage)
+          }
+        } else {
+          errorMessage = `Invalid address has been entered!`
+          logger.error(errorMessage)
+          throw new Error(errorMessage)
+        }
+      } else {
+        errorMessage = `Invalid DID has been entered!`
+        logger.error(errorMessage)
+        throw new Error(errorMessage)
       }
-      return { polygon: resolve };
+    } catch (error) {
+      logger.error(`Error occurred in resolve function ${error}`)
+      throw error
+    }
+  }
+  return { polygon: resolve }
 }
-    
 
 /**
  * Split polygon DID.
@@ -102,6 +106,6 @@ export function getResolver (): Record<string, DIDResolver> {
  * @returns Returns Split data value to polygon DID.
  */
 async function splitPolygonDid(did: string): Promise<string> {
-      const splitDidValue: string = did.split(":")[2];
-      return splitDidValue;
+  const splitDidValue: string = did.split(':')[2]
+  return splitDidValue
 }
--- tests/test.data.ts
+++ tests/test.data.ts
@@ -1,8 +1,7 @@
+export const testDid = ''
 
-export const testDid = '';
+export const privateKey = ''
 
-export const privateKey = "";
+export const url = 'https://rpc-mumbai.maticvigil.com/'
 
-export const url = "https://rpc-mumbai.maticvigil.com/";
-
-export const contractAddress = "0x8B335A167DA81CCef19C53eE629cf2F6291F2255";
+export const contractAddress = '0x8B335A167DA81CCef19C53eE629cf2F6291F2255'
