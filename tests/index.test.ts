ee77be87c582bb6a972666c147c0c7e777d4629c
Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
2023-08-17 19:25:10 +0530

    refactor: did resolver tests and utils (#11)
    * refactor: add pnpm and update package.json
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * test: update tests to use node:assert
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: separate out the utils
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    * refactor: update the polygon did regex
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>
    
    ---------
    
    Signed-off-by: Sai Ranjit Tummalapalli <sairanjit.tummalapalli@ayanworks.com>

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,34 +1,25 @@
-import { testDid } from './fixtures/test.data'
-import * as didPolygon from '../src/polygon-did-resolver'
-import { describe, it, beforeEach } from 'node:test'
+import { DIDS, testDid } from './fixtures/test.data'
+import { getResolver } from '../src'
+import { describe, it, before } from 'node:test'
 import assert from 'node:assert'
 import { DIDResolutionResult, Resolver } from 'did-resolver'
+import { createDidValidationTest } from './utils/createDidValidationTest'
 
-describe('test resolver function', () => {
+describe('polygon-did-resolver', () => {
   let resolveDidRes: DIDResolutionResult
-  beforeEach(async () => {
-    let resolver = new Resolver(
-      {
-        ...didPolygon.getResolver(),
-      },
 
-      { cache: true },
-    )
-    resolveDidRes = await resolver.resolve(testDid)
-  })
-  it('should be polygon DID for resolve DID', async () => {
-    if (testDid && testDid.split(':')[2] === 'testnet') {
-      assert.strictEqual(testDid.slice(0, 19), 'did:polygon:testnet')
-      assert.strictEqual(testDid.slice(20, 22), '0x')
-      assert.strictEqual(testDid.split(':')[3].length, 42)
-    } else {
-      assert.strictEqual(testDid.slice(0, 12), 'did:polygon')
-      assert.strictEqual(testDid.slice(12, 14), '0x')
-      assert.equal(testDid.split(':')[2].length, 42)
-    }
+  describe('Validate did', () => {
+    DIDS.forEach(createDidValidationTest)
   })
 
-  it('should get DID document', async () => {
-    assert.ok(resolveDidRes.didDocument)
+  describe('Validate did document', () => {
+    before(async () => {
+      const polygonDidResolver = getResolver()
+      let resolver = new Resolver(polygonDidResolver)
+      resolveDidRes = await resolver.resolve(testDid)
+    })
+    it('should get DID document', async () => {
+      assert.ok(resolveDidRes.didDocument)
+    })
   })
 })
