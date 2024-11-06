2c6162bb319fc13fdb3df1813abffb6813278517
Ankita Patidar <ankita.patidar@ayanworks.com>
2024-11-06 13:27:01 +0530

    feat:updated test network to ethereum for testing purpose
    Signed-off-by: Ankita Patidar <ankita.patidar@ayanworks.com>

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,9 @@
 export const networkConfig = {
   testnet: {
-    URL: 'https://rpc-amoy.polygon.technology',
-    CONTRACT_ADDRESS: '0xcB80F37eDD2bE3570c6C9D5B0888614E04E1e49E',
+    // URL: 'https://rpc-amoy.polygon.technology',
+    // CONTRACT_ADDRESS: '0xcB80F37eDD2bE3570c6C9D5B0888614E04E1e49E',
+    URL: 'https://ethereum-sepolia.blockpi.network/v1/rpc/public',
+    CONTRACT_ADDRESS: '0x7423254347AbE61F7606d0b850CAa1233Fb660bC',
   },
   mainnet: {
     URL: 'https://polygon-rpc.com',
